ce73adb5c7aea401a497f9725e854c0b95c7f739
Romu <romuald.six@gmail.com>
2017-05-03 14:12:28 +0200

    validate email and name format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
+# validation modele User
 class User < ApplicationRecord
+  # validations de format
+  validates :name, presence: true, length: { maximum: 50 }
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -12,4 +12,43 @@ class UserTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @user.valid?
   end
+
+  test "name should be present" do
+    @user.name = "    "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "    "
+    assert_not @user.valid?
+  end
+  test "name lenght test" do
+    @user.name = "a"*51
+    assert_not @user.valid?
+  end
+  test "email lenght test" do
+    @user.email = "a"*244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email format test" do
+    @user.email = "coucou@cestmoi.com"
+    assert @user.valid?
+  end
+
+  test "not valid emails" do
+    unvalid_add=%w[romu@gmail,com romuald romromrom@r@r, romuald.sox,
+                  test@test+test.com test@test..com]
+    unvalid_add.each do
+	    |mel|
+            @user.email = mel
+	    assert_not @user.valid?, "#{unvalid_add} is valid but should not"
+    end
+  end
+  test "email uniqueness" do
+    dupli_user = @user.dup
+    dupli_user.email = @user.email.upcase
+    @user.save
+    assert_not dupli_user.valid?
+  end
 end
