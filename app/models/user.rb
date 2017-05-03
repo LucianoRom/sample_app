da3ef2fa365db9a77feeba7a17d702c25d1324df
Romu <romuald.six@gmail.com>
2017-05-03 14:43:18 +0200

    password creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 # validation modele User
 class User < ApplicationRecord
   # validations de format
+  before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,54 +1,60 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
+  # test 'the truth' do
   #   assert true
   # end
 
   def setup
-    @user = User.new(name: "Test User", email: "user@example.com")
+    @user = User.new(name: 'Test User', email: 'user@example.com')
   end
 
-  test "should be valid" do
+  test 'should be valid' do
     assert @user.valid?
   end
 
-  test "name should be present" do
-    @user.name = "    "
+  test 'name should be present' do
+    @user.name = '    '
     assert_not @user.valid?
   end
 
-  test "email should be present" do
-    @user.email = "    "
+  test 'email should be present' do
+    @user.email = '    '
     assert_not @user.valid?
   end
-  test "name lenght test" do
-    @user.name = "a"*51
+  test 'name lenght test' do
+    @user.name = 'a' * 51
     assert_not @user.valid?
   end
-  test "email lenght test" do
-    @user.email = "a"*244 + "@example.com"
+  test 'email lenght test' do
+    @user.email = 'a' * 244 + '@example.com'
     assert_not @user.valid?
   end
 
-  test "email format test" do
-    @user.email = "coucou@cestmoi.com"
+  test 'email format test' do
+    @user.email = 'coucou@cestmoi.com'
     assert @user.valid?
   end
 
-  test "not valid emails" do
-    unvalid_add=%w[romu@gmail,com romuald romromrom@r@r, romuald.sox,
-                  test@test+test.com test@test..com]
-    unvalid_add.each do
-	    |mel|
-            @user.email = mel
-	    assert_not @user.valid?, "#{unvalid_add} is valid but should not"
+  test 'not valid emails' do
+    unvalid_add = %w[romu@gmail,com romuald romromrom@r@r, romuald.sox,
+                     test@test+test.com test@test..com]
+    unvalid_add.each do |mel|
+      @user.email = mel
+      assert_not @user.valid?, '#{unvalid_add} is valid but should not'
     end
   end
-  test "email uniqueness" do
+  test 'email uniqueness' do
     dupli_user = @user.dup
     dupli_user.email = @user.email.upcase
     @user.save
     assert_not dupli_user.valid?
+  end
+
+  test 'email is written in downcase' do
+    test_email = 'YouPlaBop@geniALL.COm'
+    @user.email = test_email
+    @user.save
+    assert_equal test_email.downcase, @user.reload.email
   end
 end
