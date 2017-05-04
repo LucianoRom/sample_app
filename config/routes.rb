080af1ee830ec55abdfcd6d0867f4d777793cbdf
Romu <romuald.six@gmail.com>
2017-05-04 14:27:18 +0200

    add stylesheet for profile

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
 
   root 'static_pages#home'
-   get '/help', to: 'static_pages#help'
+  get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
   get '/about', to: 'static_pages#about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :users
 end
