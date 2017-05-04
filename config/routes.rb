6e74a0f7861fd1de636fe644407a590cc6cce81b
Romu <romuald.six@gmail.com>
2017-05-04 20:35:46 +0200

    add login page

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
   get '/about', to: 'static_pages#about'
+  get '/login', to: 'session#new'
+  post '/login', to: 'session#create'
+  delete '/logout', to: 'session#destroy'
+
   resources :users
 end
