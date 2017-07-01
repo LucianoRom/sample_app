c13fcf00cc63af5e61681a20f4c6efc7fd0332ab
Romu <romuald.six@gmail.com>
2017-07-01 18:54:16 +0200

    nbjoueur devient nb_joueur

--- db/migrate/20170516092342_create_jeu_nb_joueurs.rb
+++ db/migrate/20170516092342_create_jeu_nb_joueurs.rb
@@ -2,7 +2,7 @@ class CreateJeuNbJoueurs < ActiveRecord::Migration[5.0]
   def change
     create_table :jeu_nb_joueurs do |t|
       t.references :game, foreign_key: true
-      t.references :nbjoueur, foreign_key: true
+      t.references :nb_joueur, foreign_key: true
 
       t.timestamps
     end
