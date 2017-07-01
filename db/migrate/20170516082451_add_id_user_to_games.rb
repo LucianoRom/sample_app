106205e055b4985f5afdf2950ac71fbdc1652d2a
Romu <romuald.six@gmail.com>
2017-07-01 16:21:11 +0200

    user game index

--- db/migrate/20170516082451_add_id_user_to_games.rb
+++ db/migrate/20170516082451_add_id_user_to_games.rb
@@ -2,7 +2,6 @@ class AddIdUserToGames < ActiveRecord::Migration[5.0]
   def change
     add_reference :games, :user, foreign_key: true
   end
-  add_index :games, :name
   add_index :games, [:name, :user_id]
 
 
